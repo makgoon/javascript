4d4c8bd21c0a9f9d1662e536a8d6c1e4019340b4
Kim Jae-Young <miracle013@naver.com>
2020-05-21 10:19:24 +0900

    실습
    ajax 샘플 완성

--- ajax/app.js
+++ ajax/app.js
@@ -1,18 +1,31 @@
-var express = require('express')
-  , http = require('http')
-  , app = express()
-  , server = http.createServer(app);
+var express = require('express');
+var app = express();
+var http = require('http');
+var server = http.createServer(app).listen(80);
 
-app.get('/' function (req,res){
+app.get('/', function (req,res){
   res.send('Hello /')
 });
 
-app.get('/world.html' function (req,res){
+app.get('/world.html', function (req,res){
   res.send('Hello Hello')
 });
 
-server.listen(8000, funcion(){
-  console.log(server.address().port + ' : is running');
+app.get('/sum', function (req,res){
+  var num1 = Number(req.query.num1);
+  var num2 = Number(req.query.num2);
+  console.log(num1, num2);
+  res.send(`합은 ${num1 + num2} 입니다.`);
+
 });
+
+app.get('/sumForm', function (req, res) {
+  res.sendfile("sumForm.html");
+
+});
+
+// server.listen(80, function(){
+//   console.log(server.address().port + ' : is running');
+// });
 
 //http://pyrasis.com/nodejs/nodejs-HOWTO//참고사이트
